e62b23fac01028f190f6dae477a84711826fc041
saka <sakanabil1221@gmail.com>
2025-10-27 10:07:44 +0700

    add: week_09 p1

---
+++ Week-09/kamera_flutter/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:kamera_flutter/widget/takepicture_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
