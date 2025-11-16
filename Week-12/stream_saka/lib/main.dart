369e55619956c62b0b40b87bbbec57eba4c52c76
saka <sakanabil1221@gmail.com>
2025-11-16 21:10:59 +0700

    add: p2

--- Week-12/stream_saka/lib/main.dart
+++ Week-12/stream_saka/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'stream.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -29,11 +31,28 @@ class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
 
+  int lastNumber = 0;
+  late StreamController numberStreamController;
+  late NumberStream numberStream;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Stream Saka')),
-      body: Container(decoration: BoxDecoration(color: bgColor)),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(lastNumber.toString()),
+            ElevatedButton(
+              onPressed: () => addRandomNumber(),
+              child: Text('New Random Number Saka'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -54,8 +73,34 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   @override
   void initState() {
+    numberStream = NumberStream();
+    numberStreamController = numberStream.controller;
+    Stream stream = numberStreamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    });
     super.initState();
-    colorStream = ColorStream();
-    changeColor();
   }
+
+  @override
+  void dispose() {
+    numberStreamController.close();
+    super.dispose();
+  }
+
+  void addRandomNumber() {
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+  }
+
+  // Soal 7
+  // Langkah 13: Membuat Pemicu Error
+  // Membuat sebuah fungsi baru bernama addError(). Tugas fungsi ini adalah memasukkan (inject) sebuah data error (dalam hal ini, string 'error') secara manual ke dalam stream. Ini adalah cara untuk "sengaja" membuat stream mengalami kondisi error untuk keperluan pengujian.
+  // Langkah 14: Membuat Penangkap Error
+  // Menambahkan blok .onError() pada stream.listen(). Ini adalah penangkap error.
+  // Langkah 15: Menggunakan Pemicu Error
+  // Mengubah apa yang dilakukan oleh fungsi addRandomNumber(). Fungsi ini terhubung dengan tombol 'New Random Number'
 }
