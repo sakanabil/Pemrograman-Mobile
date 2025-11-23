97b6a5bacf63972ba4bdb9a4978f8461b5b2cef8
saka <sakanabil1221@gmail.com>
2025-11-23 19:08:17 +0700

    add: p3

--- Week-13/store_data_saka/lib/model/pizza.dart
+++ Week-13/store_data_saka/lib/model/pizza.dart
@@ -1,4 +1,16 @@
+const keyId = 'id';
+const keyName = 'pizzaName';
+const keyDescription = 'description';
+const keyPrice = 'price';
+const keyImage = 'imageUrl';
+
 class Pizza {
+  final int id;
+  final String pizzaName;
+  final String description;
+  final double price;
+  final String imageUrl;
+
   Pizza({
     required this.id,
     required this.pizzaName,
@@ -7,26 +19,22 @@ class Pizza {
     required this.imageUrl,
   });
 
+  Pizza.fromJson(Map<String, dynamic> json)
+    : id = int.tryParse(json[keyId].toString()) ?? 0,
+      pizzaName = json[keyName] != null ? json[keyName].toString() : 'No name',
+      description = json[keyDescription] != null
+          ? json[keyDescription].toString()
+          : 'No description',
+      price = double.tryParse(json[keyPrice].toString()) ?? 0.0,
+      imageUrl = json[keyImage] ?? '';
+
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'pizzaName': pizzaName,
-      'description': description,
-      'price': price,
-      'imageUrl': imageUrl,
+      keyId: id,
+      keyName: pizzaName,
+      keyDescription: description,
+      keyPrice: price,
+      keyImage: imageUrl,
     };
   }
-
-  Pizza.fromJson(Map<String, dynamic> json)
-    : id = int.tryParse(json['id'].toString()) ?? 0,
-      pizzaName = (json['pizzaName']?.toString() ?? 'No name'),
-      description = (json['description']?.toString() ?? 'No description'),
-      price = double.tryParse(json['price'].toString()) ?? 0.0,
-      imageUrl = (json['imageUrl']?.toString() ?? '');
-
-  final int id;
-  final String pizzaName;
-  final String description;
-  final double price;
-  final String imageUrl;
 }
