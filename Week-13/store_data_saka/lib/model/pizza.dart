3963e71d83c02c2a15fcc088677d83d7c494764b
saka <sakanabil1221@gmail.com>
2025-11-23 19:04:19 +0700

    add: p2

--- Week-13/store_data_saka/lib/model/pizza.dart
+++ Week-13/store_data_saka/lib/model/pizza.dart
@@ -1,4 +1,12 @@
 class Pizza {
+  Pizza({
+    required this.id,
+    required this.pizzaName,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+
   Map<String, dynamic> toJson() {
     return {
       'id': id,
@@ -10,11 +18,11 @@ class Pizza {
   }
 
   Pizza.fromJson(Map<String, dynamic> json)
-    : id = json['id'],
-      pizzaName = json['pizzaName'],
-      description = json['description'],
-      price = json['price'],
-      imageUrl = json['imageUrl'];
+    : id = int.tryParse(json['id'].toString()) ?? 0,
+      pizzaName = (json['pizzaName']?.toString() ?? 'No name'),
+      description = (json['description']?.toString() ?? 'No description'),
+      price = double.tryParse(json['price'].toString()) ?? 0.0,
+      imageUrl = (json['imageUrl']?.toString() ?? '');
 
   final int id;
   final String pizzaName;
