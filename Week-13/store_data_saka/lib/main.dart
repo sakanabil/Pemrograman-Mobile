84ea0b442bac300632e7893ae68960f95db158b8
saka <sakanabil1221@gmail.com>
2025-11-23 19:29:55 +0700

    add: p6

--- Week-13/store_data_saka/lib/main.dart
+++ Week-13/store_data_saka/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +29,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late File myFile;
+  String fileText = '';
+
   int appCounter = 0;
 
   String documentsPath = '';
   String tempPath = '';
+
+  Future<bool> writeFile() async {
+    try {
+      await myFile.writeAsString('Saka Nabil, 2341720108');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPaths().then((_) {
+      myFile = File('$documentsPath/pizzas.txt');
+      writeFile();
+    });
+  }
+
+  Future<bool> readFile() async {
+    try {
+      // Read the file.
+      String fileContent = await myFile.readAsString();
+      setState(() {
+        fileText = fileContent;
+      });
+      return true;
+    } catch (e) {
+      // On error, return false.
+      return false;
+    }
+  }
 
   Future<void> readAndWritePreference() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -61,12 +97,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    getPaths();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Path Provider')),
@@ -74,7 +104,12 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Text('Doc path: $documentsPath'),
-          Text('Temp path $tempPath'),
+          Text('Temp path: $tempPath'),
+          ElevatedButton(
+            child: const Text('Read File'),
+            onPressed: () => readFile(),
+          ),
+          Text(fileText),
         ],
       ),
     );
